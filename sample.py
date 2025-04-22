02794468b5956466a94c6d2a043e919c4fb1c01b
Sandhiya S <sandhiyangp13012006@gmail.com>
2025-04-22 21:31:01 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,6 +2,8 @@ def factorial(n):
 n =input("please enter you number")
 if n==0:
 return 1
+elif n<0:
+    print(" Factorial is not valid for Negative Numbers")
 else:
 return n*factorial(n-1)
 print (factorial(n))
